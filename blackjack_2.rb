e2a1caa2a45f34b5fcab8991c87a7b7e3b18bec9
DeShawn <dcousette@gmail.com>
2015-03-09 23:55:22 +0000

    Clean up code syntax

--- blackjack_2.rb
+++ blackjack_2.rb
@@ -1,8 +1,8 @@
 def calculate_total(cards) #[["H", "2"], ["D, K"]]...
-  arr = cards.map {|a| a[1] }
+  card_values_array = cards.map {|card| card[1] }
   total = 0 
   
-  arr.each do |value|
+  card_values_array.each do |value|
     if value == "A"
       total += 11
     elsif value.to_i == 0
@@ -13,7 +13,7 @@ def calculate_total(cards) #[["H", "2"], ["D, K"]]...
   end
   
   #correct for aces
-  arr.select {|a| a == "A"}.count.times do 
+  card_values_array.select {|value| value == "A"}.count.times do 
     total -= 10 if total > 21  
   end
   
@@ -29,26 +29,26 @@ deck = suits.product(cards)
 deck.shuffle!
 
 #deal cards 
-mycards = []
-dealercards = []
+my_cards = []
+dealer_cards = []
 
-mycards << deck.pop
-dealercards << deck.pop
-mycards << deck.pop
-dealercards << deck.pop
+my_cards << deck.pop
+dealer_cards << deck.pop
+my_cards << deck.pop
+dealer_cards << deck.pop
 
-mycards_total = calculate_total(mycards)
-dealercards_total = calculate_total(dealercards)
+my_cards_total = calculate_total(my_cards)
+dealer_cards_total = calculate_total(dealer_cards)
 
 puts "Welcome to Blackjack!"
-puts "Your cards are: #{mycards[0]} and #{mycards[1]}. For a total of: #{mycards_total}"
-puts "The Dealer's cards: #{dealercards[0]} and #{dealercards[1]}. For a total of: #{dealercards_total}"
+puts "Your cards are: #{my_cards[0]} and #{my_cards[1]}. For a total of: #{my_cards_total}"
+puts "The Dealer's cards: #{dealer_cards[0]} and #{dealer_cards[1]}. For a total of: #{dealer_cards_total}"
 
-if mycards_total == 21
+if my_cards_total == 21
   puts "Congratulations you lucky dog! You've got Blackjack!"
 end 
 
-while mycards_total < 21
+while my_cards_total < 21
   puts "What would you like to do: Hit(1)? or Stay(2)?"
   hit_or_stay = gets.chomp
   
@@ -59,36 +59,36 @@ while mycards_total < 21
   
   if hit_or_stay == "2"
     puts "You chose to stay..."
-    puts "Your score is: #{mycards_total}"
+    puts "Your score is: #{my_cards_total}"
     break
   end 
   
   new_card = deck.pop
-  mycards << new_card
-  mycards_total = calculate_total(mycards)
+  my_cards << new_card
+  my_cards_total = calculate_total(mycards)
   puts "You were dealt a: #{new_card}"
-  puts "Your score is: #{mycards_total}"
+  puts "Your score is: #{my_cards_total}"
 end
 
-if mycards_total > 21 
+if my_cards_total > 21 
   puts "Sorry partner, you've busted! Loser."
   exit 
 end
 
 #dealer turn 
-if dealercards_total == 21
+if dealer_cards_total == 21
   puts "Dealer's got Blackjack! so... You lose!"
   exit
 end 
 
-while dealercards_total < 17 
+while dealer_cards_total < 17 
   new_card = deck.pop
   puts "Dealer was dealt a: #{new_card}"
-  dealercards << new_card
-  dealercards_total = calculate_total(dealercards)
-  puts "The dealer's score is: #{dealercards_total}"
+  dealer_cards << new_card
+  dealer_cards_total = calculate_total(dealer_cards)
+  puts "The dealer's score is: #{dealer_cards_total}"
   
-  if dealercards_total > 21 
+  if dealer_cards_total > 21 
     puts "Dealer busts! YOU... WIN!!!"
     exit
   end 
@@ -96,18 +96,18 @@ end
 
 #compare cards 
 puts "Your cards are:"
-mycards.each do |card|
+my_cards.each do |card|
   puts "=> #{card}"
 end 
 
 puts "The dealer's cards are:"
-dealercards.each do |card|
+dealer_cards.each do |card|
   puts "=> #{card}"
 end
 
-if mycards_total > dealercards_total
+if my_cards_total > dealer_cards_total
   puts "Oh... you win..."
-elsif dealercards_total > mycards_total 
+elsif dealer_cards_total > my_cards_total 
   puts "DEALER WINS! So sorry, you lost. again."
 else
   puts "It's a tie."
